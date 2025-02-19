d653e3c75bdecdbeea4bbf98a17b45cd48c929de
Isabel Boufarah <bisabel@wustl.edu>
2025-02-19 13:20:11 -0600

    inline

--- src/components/one.js
+++ src/components/one.js
@@ -3,7 +3,7 @@ import styles from '../styles/one.module.css'
 export default function One() {
 
     return (<div style={{padding: '56px 24px 56px 0', height: '2000px', backgroundColor: 'pink'}}>
-        <div  className={styles.progress}></div>
+        <div  className={styles.progress} style={{animationTimeline: 'scroll()'}}></div>
         <div
             style={{}}
         ></div>
