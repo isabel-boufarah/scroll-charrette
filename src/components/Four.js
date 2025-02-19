53f945a7e769ebe6dc0185ec58f8da488d2cf180
Isabel Boufarah <bisabel@wustl.edu>
2025-02-19 15:47:50 -0600

    fix

--- src/components/Four.js
+++ src/components/Four.js
@@ -3,9 +3,9 @@ import styles from '../styles/four.module.css'
 export default function Four() {
 
     return (<div style={{height: '3000px'}}>
-        <div className={styles.div1} styles={{animationTimeline: 'scroll()'}}></div>
-        <div className={styles.div2} styles={{animationTimeline: 'scroll()'}}></div>
-        <div className={styles.div3} styles={{animationTimeline: 'scroll()'}}></div>
+        <div className={styles.div1} style={{animationTimeline: 'scroll()'}}></div>
+        <div className={styles.div2} style={{animationTimeline: 'scroll()'}}></div>
+        <div className={styles.div3} style={{animationTimeline: 'scroll()'}}></div>
     </div>
         
     )
--- src/components/TwentyTwo.js
+++ src/components/TwentyTwo.js
@@ -2,8 +2,8 @@ import styles from '../styles/twentytwo.module.css'
 
 export default function TwentyTwo() {
     return (<div style={{height: '3000px'}}>
-        <div className={styles.div1} styles={{animationTimeline: 'scroll()'}}></div>
-        <div className={styles.div2} styles={{animationTimeline: 'scroll()'}}></div>
-        <div className={styles.div3} styles={{animationTimeline: 'scroll()'}}></div>
+        <div className={styles.div1} style={{animationTimeline: 'scroll()'}}></div>
+        <div className={styles.div2} style={{animationTimeline: 'scroll()'}}></div>
+        <div className={styles.div3} style={{animationTimeline: 'scroll()'}}></div>
     </div>)
 }
